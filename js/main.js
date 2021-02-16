f8b89bcfe3c81c3684e8ce636399bdd98966f2f5
miloszKubiak <milypan69@gmail.com>
2021-02-16 12:15:27 +0100

    Added a function that changes the color of the burger depending on the section

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ const allSections = document.querySelectorAll('.section');
 const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
-    nav.classList.toggle('nav--active')
+    nav.classList.toggle('nav--active');
+
+    navBtnBars.classList.remove('black-bars-color');
     
     allNavItems.forEach(item => {
         item.addEventListener('click', () => {
@@ -28,10 +30,23 @@ const handleNavItemsAnimation = () => {
     })
 }
 
+const handleObserver = () => {
+    const currentSection = window.scrollY;
+
+    allSections.forEach(section => {
+        if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.add('black-bars-color')
+        } else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.remove('black-bars-color')
+        }
+    })
+}
+
 const handleCurrentYear = () => {
     const year = (new Date).getFullYear();
     footerYear.innerText = year;
 }
 
 handleCurrentYear();
-navBtn.addEventListener('click', handleNav)
+navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleObserver);
